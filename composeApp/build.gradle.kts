ff027d0297a9136a6f5c4143540e505d8fe3f07a
Fire Forest Souls <stepan.sniff@gmail.com>
2026-01-09 12:01:51 +0300

    v1.1.5 Update App Version
    🔢 Обновлена версия приложения до v1.1.5 (8)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -109,8 +109,8 @@ android {
         applicationId = "fireforestsoul.levelupsoul"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "1.1.4"
+        versionCode = 8
+        versionName = "1.1.5"
         setProperty("archivesBaseName", "LevelUpSoul-v$versionName")
     }
     packaging {
@@ -181,7 +181,7 @@ compose.desktop {
             )
 
             packageName = "LevelUpSoul"
-            packageVersion = "1.1.4"
+            packageVersion = "1.1.5"
 
             linux {
                 iconFile.set(project.file("src/desktopMain/resources/app_icon.png"))
